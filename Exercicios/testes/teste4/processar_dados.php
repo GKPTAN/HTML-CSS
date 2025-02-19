6a8c14441e06ea9f6c4aa0284a44be9199f22bd9
GKPTAN <146596464+GKPTAN@users.noreply.github.com>
2025-02-19 00:25:05 -0300

    teste
    - criando um formulário que envia os dados para um banco de dados usando o php, xampp e o mysql.

---
+++ Exercicios/testes/teste4/processar_dados.php
@@ -0,0 +1,35 @@
+<?php
+$name = $_POST['names'];
+$age = $_POST['ages'];
+$gender = $_POST['genres'];
+$email = $_POST['emails'];
+if ($_POST['passwords'][0] ===  $_POST['passwords'][1]) {
+    $password = $_POST['passwords'][0];
+} else {
+    echo "As senhas não coincidem";
+    return;
+}
+$data_atual = date('d/m/Y');
+$hora_atual = date('H:i:s');
+
+$server = 'localhost';
+$root = 'root';
+$senha = '';
+$database = 'gym_users';
+
+$conn = new mysqli($server, $root, $senha, $database);
+
+if($conn->connect_error){
+    die("Falha ao se comunicar com o banco de dados: ".$conn->connect_error);
+}
+
+$smtp = $conn->prepare("INSERT INTO users (name, age, gender, email, password, date, hours) VALUES (?,?,?,?,?,?,?)");
+$smtp->bind_param("sisssss", $name, $age, $gender, $email, $password, $data_atual, $hora_atual);
+if($smtp->execute()) {
+    echo "dados enviados com sucesso!";
+} else {
+    echo "Erro no envio dos dados".$smtp->error;
+}
+$smtp->close();
+$conn->close();
+?>
